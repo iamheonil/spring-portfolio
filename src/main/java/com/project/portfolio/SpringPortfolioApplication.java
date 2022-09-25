0de53952df12de2286b6ef7ee507631c0bf710c2
eunhyeok <ceh2805@gmail.com>
2022-09-25 23:23:16 +0900

    #3 - 스프링부트 프로젝트 초기설정
    각종 패키지 세분화
     - controller, repository, service, domain, dto, config
    MainController 생성
    index.html 파일 생성
    hello world까지 띄워지도록 세팅

---
+++ src/main/java/com/project/portfolio/SpringPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.project.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPortfolioApplication.class, args);
+    }
+
+}
